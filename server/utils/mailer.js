5c37d1f4b1b3a64e3ed615e505fad686404ac71c
Satyam kumar Ray <satyamkumarroy216@gmail.com>
2026-04-13 07:22:53 +0530

    fix email issue

--- server/utils/mailer.js
+++ server/utils/mailer.js
@@ -1,12 +1,18 @@
 const nodemailer = require("nodemailer");
 
-// Create mail transporter using environment variables
 const transporter = nodemailer.createTransport({
     service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false, 
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
     },
+    
+    tls: {
+        rejectUnauthorized: false
+    }
 });
 
 module.exports = transporter;
