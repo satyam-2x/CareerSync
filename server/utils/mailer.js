fad833b142f2663de7b67a3664ba541e66dcf152
Satyam kumar Ray <satyamkumarroy216@gmail.com>
2026-04-13 07:44:32 +0530

    fix nodemailer

--- server/utils/mailer.js
+++ server/utils/mailer.js
@@ -1,18 +1,17 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-    service: "gmail",
     host: "smtp.gmail.com",
-    port: 587,
-    secure: false, 
+    port: 465, // Render par 465 + secure: true aksar 587 se behtar chalta hai
+    secure: true, 
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
     },
-    
-    tls: {
-        rejectUnauthorized: false
-    }
+    // Ye timeouts Render ke slow network ko handle karenge
+    connectionTimeout: 30000, // 30 seconds
+    greetingTimeout: 30000,
+    socketTimeout: 30000,
 });
 
 module.exports = transporter;
